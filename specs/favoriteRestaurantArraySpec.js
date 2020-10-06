495116f1326916af7fa36a6dbfa9dd6c4d854979
luissagit <casualulie@gmail.com>
2020-10-06 12:10:05 +0700

    contract test implementation

---
+++ specs/favoriteRestaurantArraySpec.js
@@ -0,0 +1,34 @@
+import {itActsAsFavoriteRestaurantModel} from './contracts/favoriteRestaurantContract';
+
+let favoriteRestaurant = [];
+
+const FavoriteRestaurantArray = {
+	getRestaurant(id) {
+		if (!id) {
+			return;
+		}
+
+		return favoriteRestaurant.find((restaurant) => restaurant.id == id);
+	},
+
+	getAllRestaurant() {
+		return favoriteRestaurant;
+	},
+
+	putRestaurant(restaurant) {
+		if (!restaurant.hasOwnProperty('id')) {
+			return;
+		}
+
+		favoriteRestaurant.push(restaurant);
+	},
+
+	deleteRestaurant(id) {
+		favoriteRestaurant = favoriteRestaurant.filter((restaurant) => restaurant.id != id);
+	},
+};
+
+describe('Favorite Restaurant Array Contract Test Implementation', () => {
+	afterEach(() => favoriteRestaurant = []);
+	itActsAsFavoriteRestaurantModel(FavoriteRestaurantArray);
+});
