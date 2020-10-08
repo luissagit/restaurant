3791bed7a8bb5b4ac099f9c0217c2901f2761e81
luissagit <casualulie@gmail.com>
2020-10-08 16:53:38 +0700

    auto testing

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -33,6 +33,37 @@ Scenario('liking one restaurant', async (I) => {
 	assert.strictEqual(firstRestoName, likedRestoName);
 });
 
+Scenario('unliking one restaurant', async (I) => {
+	I.see('Tidak ada restoran untuk ditampilkan', '.restaurant-item__not__found');
+
+	I.amOnPage('/');
+
+	I.seeElement('.restaurant__name');
+
+	const firstResto = locate('.restaurant__name').first();
+	const firstRestoName = await I.grabTextFrom(firstResto);
+
+	I.click(firstResto);
+
+	I.seeElement('#likeButton');
+	I.click('#likeButton');
+
+	I.amOnPage('/#/favorite');
+	I.seeElement('.restaurant-item');
+	const likedRestoName = await I.grabTextFrom('.restaurant__name');
+
+	assert.strictEqual(firstRestoName, likedRestoName);
+
+	I.seeElement('.restaurant__name');
+	I.click(locate('.restaurant__name').first());
+
+	I.seeElement('#likeButton');
+	I.click('#likeButton');
+
+	I.amOnPage('/#/favorite');
+	I.see('Tidak ada restoran untuk ditampilkan', '.restaurant-item__not__found');
+});
+
 Scenario('searching favorite restaurant', async (I) => {
 	I.see('Tidak ada restoran untuk ditampilkan', '.restaurant-item__not__found');
 
